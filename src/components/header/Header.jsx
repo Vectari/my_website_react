356afa5a9d3b512f4dcdc6089075fdf84c301c69
Vectari <mateusz.majer996@gmail.com>
2023-02-12 20:18:58 +0100

    add transparent my-photo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/my-photo.jpg'
+import ME from '../../assets/my-photo-transparent.PNG'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
