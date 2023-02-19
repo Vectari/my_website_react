ca27b6345931bc3e542e5ed2653681658f23e6c8
Vectari <mateusz.majer996@gmail.com>
2023-02-19 14:36:47 +0100

    add portfolio content

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './portfolio.css'
+import serduszkadwa from '../../assets/serduszka-dwa.png'
+import jamming from '../../assets/jamming.png'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={serduszkadwa} alt="" />
+          </div>
+          <div className="portfolio__item-image"></div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/Vectari/serduszka-dwa" target="_blank" className='btn'>GitHub</a>
+          <a href="https://serduszkadwa.pl" target="_blank" className="btn btn-primary">Serduszka Dwa</a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={jamming} alt="" />
+          </div>
+          <div className="portfolio__item-image"></div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/Vectari/Jamming" target="_blank" className='btn'>GitHub</a>
+          <a href="https://vectari.github.io/Jamming/" target="_blank" className="btn btn-primary">Jammming</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
