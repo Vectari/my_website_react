a5252cbd0847de4df7ff461a57d59f061b3e06ae
Vectari <mateusz.majer996@gmail.com>
2023-02-19 14:01:09 +0100

    add experience section style

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -13,58 +13,68 @@ const Experience = () => {
           <h3>Frontend Development</h3>
           <div className="experience__content">
             <artice className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>HTML</h4>
-              <small className='text-light'>Experienced</small>
-              <br />
+              <BsFillCheckCircleFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </artice>
             <artice className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>CSS</h4>
-              <small className='text-light'>Intermediate</small>
-              <br />
+              <BsFillCheckCircleFill className='experience__details-icon' />
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
             </artice>
             <artice className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>JavaScript</h4>
-              <small className='text-light'>Experienced</small>
-              <br />
+              <BsFillCheckCircleFill className='experience__details-icon' />
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </artice>
             <artice className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>React</h4>
-              <small className='text-light'>Experienced</small>
-              <br />
+              <BsFillCheckCircleFill className='experience__details-icon' />
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </artice>
           </div>
         </div>
-        
+
         <div className="experience__backend">
         <h3>Other Skills</h3>
+        <div className="experience__content">
         <artice className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>GIT</h4>
-              <small className='text-light'>Experienced</small>
-              <br />
+              <BsFillCheckCircleFill className='experience__details-icon' />
+              <div>
+                <h4>GIT</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </artice>
             <artice className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>GitHub</h4>
-              <small className='text-light'>Intermediate</small>
-              <br />
+              <BsFillCheckCircleFill className='experience__details-icon' />
+              <div>
+                <h4>GitHub</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
             </artice>
             <artice className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>Team Work</h4>
-              <small className='text-light'>Experienced</small>
-              <br />
+              <BsFillCheckCircleFill className='experience__details-icon' />
+              <div>
+                <h4>Team Work</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </artice>
             <artice className="experience__details">
-              <BsFillCheckCircleFill />
-              <h4>Self organization</h4>
-              <small className='text-light'>Experienced</small>
-              <br />
+              <BsFillCheckCircleFill className='experience__details-icon' />
+              <div>
+                <h4>Self organization</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </artice>
+          </div>
         </div>
       </div>
     </section>
