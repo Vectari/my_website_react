31a1a1f309dc282620568404fd794f9d1f9dd754
Vectari <mateusz.majer996@gmail.com>
2023-02-20 23:23:50 +0100

    begin add content to contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,21 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <h4>Email</h4>
+            <h5>mateusz.majer996@gmail.com</h5>
+            <a href="mailto:mateusz.majer996@gmail.com">Send a message</a>
+          </article>
+        </div>
+        <form action=""></form>
+      </div>
+    </section>
   )
 }
 
