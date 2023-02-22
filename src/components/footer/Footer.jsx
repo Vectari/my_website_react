1090df1147fcbb70fb1c04d1ad42cd4ec920c71b
Vectari <mateusz.majer996@gmail.com>
2023-02-22 21:21:41 +0100

    add footer content

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Mateusz Majer</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/mateusz-majer/" target="_blank"><BsLinkedin /></a>
+        <a href="https://github.com/Vectari" target="_blank"><FaGithub /></a>
+      </div>
+    </footer>
   )
 }
 
